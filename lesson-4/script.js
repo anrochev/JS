26a634e9169f7390586a6294545f00ec83ad4a24
anrochev <roch@inbox.ru>
2021-04-04 17:07:48 +0300

    Занятие 4. Работа с массивами

---
+++ lesson-4/script.js
@@ -0,0 +1,42 @@
+const baseMedia = [{
+    name: 'Шерлок',
+    type: 'tvshow',
+    year: '2016',
+    episodes: [{
+        name: 'Знакомство с Шерлоком',
+        episodeNumber: '10'
+    }, {
+        name: 'Собака Баскервили',
+        episodeNumber: '1'
+    }]
+}, {
+    name: 'Шрек',
+    year: '2014',
+    type: 'movie'
+}, {
+    name: 'Декстер',
+    type: 'tvshow',
+    year: '2010',
+    episodes: [{
+        name: 'Побег',
+        episodeNumber: '2'
+    }, {
+        name: 'Жизнь после',
+        episodeNumber: '3'
+    }, {
+        name: 'Первая история декстера',
+        episodeNumber: '1'
+    }]
+}, 
+];
+
+let mediaNames = myMap(baseMedia, (media) => {
+    return media.name
+});
+console.log(mediaNames);
+
+function myMap(incomingArray, func) {
+    const resultArray = [];
+    const reducer = (accumulator, currentValue) => accumulator.concat(func(currentValue));
+    return incomingArray.reduce(reducer, resultArray);
+}
