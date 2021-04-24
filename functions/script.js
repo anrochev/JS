cc6bffbee9af1114c4167c3a848f87a2549d636f
anrochev <roch@inbox.ru>
2021-04-24 12:02:46 +0300

    убрал лишний файл css и отформатировал код

--- functions/script.js
+++ functions/script.js
@@ -1,16 +1,16 @@
 //реализовать функцию bind
 var sum = function() {
-  return [].reduce.call(arguments, function(result, current) {
-    return result + current;
-  }, this.sum);
+    return [].reduce.call(arguments, function(result, current) {
+        return result + current;
+    }, this.sum);
 };
 var bind = function(func, context) {
     var bindArgs = [].slice.call(arguments, 2);
     return function() {
-      var funcArgs = [].slice.call(arguments);
-      return func.apply(context, bindArgs.concat(funcArgs));
+        var funcArgs = [].slice.call(arguments);
+        return func.apply(context, bindArgs.concat(funcArgs));
     };
-  };
+};
 
-  var bindedSum = bind(sum, {sum: 20}, 30, 40);
+var bindedSum = bind(sum, { sum: 20 }, 30, 40);
 console.log(bindedSum(50, 60, 70)); // 270
